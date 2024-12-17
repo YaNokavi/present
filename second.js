93e4b294cef7fe502e8f3eca32aab9b8318cf48c
Андрей Белов <andrejbelov@MacBook-Air-Andrej-3.local>
2024-12-17 23:51:15 +0300

    add gallery

--- second.js
+++ second.js
@@ -1,8 +1,3 @@
-document.getElementById("heart").addEventListener("click", function() {
-  window.location.href = "second.html"
-})
-
-
 function createSnowflake() {
   const snowflake = document.createElement('div');
   snowflake.classList.add('snowflake');
@@ -19,15 +14,15 @@ function createSnowflake() {
   document.querySelector('.snow-container').appendChild(snowflake);
   
   // Анимация падения
-  const fallDuration = Math.random() * (5 - 3) + 3; // Случайное время падения от 3 до 5 секунд
+  const fallDuration = Math.random() * (5 - 3) + 10; // Случайное время падения от 3 до 5 секунд
   setTimeout(() => {
       snowflake.style.transition = `top ${fallDuration}s linear`; // Случайное время падения
-      snowflake.style.top = window.innerHeight + 'px'; // Падение до низа экрана
+      snowflake.style.top = window.innerHeight + 2000 + 'px'; // Падение до низа экрана
       
       // Удаляем снежинку после анимации с небольшой задержкой
       setTimeout(() => {
           snowflake.remove();
-      }, fallDuration * 1000 + 10000); // Задержка на полсекунды перед удалением
+      }, fallDuration * 1000 + 1000); // Задержка на полсекунды перед удалением
       
   }, Math.random() * (1000 - 500) + 500); // Задержка перед началом падения
 }
@@ -35,3 +30,20 @@ function createSnowflake() {
 // Создаем снежинки каждые полсекунды
 setInterval(createSnowflake, 100);
 
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	const showSlider = new Swiper('.showcase-carousel', {
+		loop: true,
+		slidesPerView: 3,
+		speed: 1000,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.showcase-navigation__next',
+			prevEl: '.showcase-navigation__prev'
+		}
+	})
+
+	// document.querySelector('video').playbackRate = 2
+
+})
